9343c5e5fc265d22ba54ca928f6a50f1757d3cbf
오승훈 <120100354+os-hoon@users.noreply.github.com>
2024-05-15 23:29:04 +0900

    [feature] FriendList
    친구 페이지 코드 구현

---
+++ server/src/main/java/com/my/kde_db/dto/FriendInfo.java
@@ -0,0 +1,11 @@
+package com.my.kde_db.dto;
+
+import lombok.Data;
+
+@Data
+public class FriendInfo {
+    private int number = 0;
+    private String name = null;
+    private String image = null;
+    private String nickname =null;
+}
