6d9352249109c1b654a9adeabada093291ef4fa1
Dongeon <dogneon18@gmail.com>
2024-06-03 01:09:58 +0900

    dd

--- server/src/main/java/com/my/kde_db/config/SecurityConfig.java
+++ server/src/main/java/com/my/kde_db/config/SecurityConfig.java
@@ -3,17 +3,15 @@ package com.my.kde_db.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .cors().and() // CORS 설정 추가
+                .csrf().disable() // CSRF 보호 비활성화
                 .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+                .anyRequest().permitAll(); // 모든 경로를 인증 없이 접근 가능하도록 설정
     }
 }
---
+++ server/src/main/java/com/my/kde_db/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.my.kde_db.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://miniwapp.netlify.app")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                )
+                .allowCredentials(true);
+    }
+}
