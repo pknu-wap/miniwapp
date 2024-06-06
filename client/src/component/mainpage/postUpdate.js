c2a30833f1fa42378cea2b60b4bc7d49b7097647
Do Hyeon Seok <dohyeondol@gmail.com>
2024-06-07 05:13:16 +0900

    [feature] 게시물 클릭 이벤트 기능 구현
    드디어 버그 해결했습니다.
    
    이전화면 버튼을 눌렀을때 2번정도 같은 화면을 띄우는 이슈가 있지만 추후 해결하도록하겠습니다.

--- client/src/component/mainpage/postUpdate.js
+++ client/src/component/mainpage/postUpdate.js
@@ -58,22 +58,7 @@ const TableRow = styled.tr`
 function PostUpdate() {
   const [posts, setPosts] = useState([]);
   const [sortOrder, setSortOrder] = useState('new');
-  const [userNumber, setUserNumber] = useState(null);
-  const [userNumbers, setUserNumbers] = useState({});
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchUserNumber = async () => {
-      try {
-        const response = await API.get('mainpage/user', { withCredentials: true }); // 현재 로그인한 사용자의 정보 조회
-        setUserNumber(response.data.number); // 현재 로그인한 사용자의 번호 설정
-      } catch (error) {
-        console.error('Error fetching user number:', error); //아오
-      }
-    };
-
-    fetchUserNumber();
-  }, []);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -81,20 +66,6 @@ function PostUpdate() {
         const response = await API.get(`mainpage/${sortOrder === 'new' ? 'new_post' : 'hot_post'}`, { withCredentials: true });
         const postsData = response.data;
         setPosts(postsData);
-
-        const userNames = [...new Set(postsData.map(post => post.name))];
-        const friendNumbersData = {};
-
-        const friendListResponse = await API.get('friendlist', { withCredentials: true });
-        const friendList = friendListResponse.data.data;
-
-        friendList.forEach(friend => {
-          if (userNames.includes(friend.name)) {
-            friendNumbersData[friend.name] = friend.number;
-          }
-        });
-
-        setUserNumbers(friendNumbersData);
       } catch (error) {
         console.error('Error fetching posts:', error);
       }
@@ -103,13 +74,8 @@ function PostUpdate() {
     fetchPosts();
   }, [sortOrder]);
 
-  const handlePostClick = (userName, postNumber) => {
-    const userNumber = userNumbers[userName];
-    if (userNumber) {
-      navigate(`/mypage/${userNumber}/notice/${postNumber}`);
-    } else {
-      console.error(`User number for ${userName} not found.`);
-    }
+  const handlePostClick = (userNumber, postNumber) => {
+    navigate(`/mypage/${userNumber}/notice/${postNumber}`);
   };
 
   return (
@@ -130,10 +96,10 @@ function PostUpdate() {
         <tbody>
           {posts.map((post) => (
             <TableRow key={post.number}>
-              <TableCellClick onClick={() => handlePostClick(post.name, post.number)}>
+              <TableCellClick onClick={() => handlePostClick(post.userNumber, post.number)}>
                 {post.title}
               </TableCellClick>
-              <TableCellClick onClick={() => handlePostClick(post.name, post.number)}>
+              <TableCellClick onClick={() => handlePostClick(post.userNumber, post.number)}>
                 {post.name}
               </TableCellClick>
               <TableCellNonClick>{post.viewCount}</TableCellNonClick>
