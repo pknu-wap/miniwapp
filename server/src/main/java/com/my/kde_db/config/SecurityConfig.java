6b99c47a9d0b0535b92a99139fae43c742ce9a35
Siyun Park <eqeee@naver.com>
2024-08-18 00:11:35 +0900

    [fix] 세션설정 수정 import문 추가

--- server/src/main/java/com/my/kde_db/config/SecurityConfig.java
+++ server/src/main/java/com/my/kde_db/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 import javax.servlet.http.HttpServletResponse;
 
