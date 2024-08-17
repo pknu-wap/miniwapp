3cde02171bb9df3624c255b19e510343f7c1b7a6
시윤 <eqeee@naver.com>
2024-08-18 01:12:15 +0900

    [fix]폼로그인 권한,cors수정

--- server/src/main/java/com/my/kde_db/config/WebConfig.java
+++ server/src/main/java/com/my/kde_db/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://miniwapp.netlify.app")
+                .allowedOrigins("http://localhost:3000", "https://miniwapp.netlify.app","https://miniwappi.shop")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),
