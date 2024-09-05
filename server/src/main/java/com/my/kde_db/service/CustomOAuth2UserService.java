0094932e374afd2362413188182c9a4db61e6250
오승훈 <120100354+os-hoon@users.noreply.github.com>
2024-09-05 17:06:39 +0900

    [fix]
    카카오 로그인 고치는중

--- server/src/main/java/com/my/kde_db/service/CustomOAuth2UserService.java
+++ server/src/main/java/com/my/kde_db/service/CustomOAuth2UserService.java
@@ -39,7 +39,6 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         String password = UUID.randomUUID().toString(); // 임의의 비밀번호 설정
         String name = profile != null && profile.get("nickname") != null ? profile.get("nickname").toString() : "Kakao User"; // 기본 이름 설정
         String birthday = kakaoAccount != null && kakaoAccount.get("birthday") != null ? kakaoAccount.get("birthday").toString() : null;
-        int state=  1;
 
         // 사용자 정보 처리
         User user = userMapper.findById(id);
@@ -52,7 +51,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             userMapper.save(user);
         }
 
-        user.setState(state);
+        user.setState(1);
         userMapper.savestate(user);
 
         session.setAttribute("me", user);
