b4524ecf46c206ba4cfdf95ddbb8034682b2fd8b
시윤 <eqeee@naver.com>
2024-09-06 11:09:44 +0900

    [fix] 로그아웃 수정
    mapper를 주입하는 방식으로 수정

--- server/src/main/java/com/my/kde_db/config/CustomLogoutHandler.java
+++ server/src/main/java/com/my/kde_db/config/CustomLogoutHandler.java
@@ -1,5 +1,6 @@
 package com.my.kde_db.config;
 
+import com.my.kde_db.dao.UserMapper;
 import com.my.kde_db.service.UserService;
 import com.my.kde_db.vo.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,42 +18,42 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 @Component
 public class CustomLogoutHandler implements LogoutHandler {
-    private final UserService userService;
     private final String kakaoLogoutUrl = "https://kapi.kakao.com/v1/user/logout";
+    private final HttpSession session;
+
+    private final UserMapper userMapper;  // UserMapper 필드 추가
 
     @Autowired
-    public CustomLogoutHandler(@Lazy UserService userService) {
-        this.userService = userService;
+    public CustomLogoutHandler(HttpSession session, UserMapper userMapper) {  // UserMapper 생성자 주입
+        this.session = session;
+        this.userMapper = userMapper;
     }
-
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        HttpSession session = request.getSession(false); // 현재 세션 가져오기
-        if (session != null) {
-            User logoutUser = (User) session.getAttribute("me");
-            if (logoutUser != null) {
-                logoutUser.setState(0);
-                userService.savestate(logoutUser);
-            }
+        User logoutUser = (User) session.getAttribute("me");
+        if (logoutUser != null) {
+            logoutUser.setState(0);
+            userMapper.savestate(logoutUser);
+            System.out.println("로그아웃 상태 저장된 유저: "+logoutUser.getName());
+        }
 
-            String accessToken = (String) session.getAttribute("accessToken");
-            if (authentication != null && accessToken != null) {
-                // 카카오 로그아웃 API 호출
-                try {
-                    URL url = new URL(kakaoLogoutUrl);
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("Authorization", "Bearer " + accessToken);
-                    int responseCode = conn.getResponseCode(); // API 호출
+        String accessToken = (String) session.getAttribute("accessToken");
+        if (authentication != null && accessToken != null) {
 
-                    if (responseCode == HttpURLConnection.HTTP_OK) {
-                        System.out.println("카카오 로그아웃 성공");
-                    } else {
-                        System.out.println("카카오 로그아웃 실패, 응답 코드: " + responseCode);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
+            // 카카오 로그아웃 API 호출
+            try {
+                URL url = new URL(kakaoLogoutUrl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+                int responseCode = conn.getResponseCode(); // API 호출
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    System.out.println("카카오 로그아웃 성공");
+                } else {
+                    System.out.println("카카오 로그아웃 실패, 응답 코드: " + responseCode);
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
